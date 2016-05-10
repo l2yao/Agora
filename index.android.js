0a85fe0cd462cfd41bd83754431d3f32764eb520
Shan <shan.du.sdu@gmail.com>
2016-05-10 01:42:28 -0400

    step 1: displaying fake data, with ListView

--- index.android.js
+++ index.android.js
@@ -2,59 +2,179 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-var Contacts = require('react-native-contacts');
+import React, { 
+	Component,
+} from 'react';
 
-Contacts.getAll((err, contacts) => {
-  if(err && err.type === 'permissionDenied'){
-    // x.x
-  } else {
-    console.log(contacts);
-  }
-});
-
-import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry, 
+	ListView, 
+	StyleSheet, 
+	Text, 
+	View, 
 } from 'react-native';
 
-class agora extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var ContactsData = require('react-native-contacts');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+var FAKE_DATA = [
+	{
+		recordID : 1,
+		familyName : "Smith",
+		givenName : "John",
+		phoneNumbers : [
+			{
+				label : "mobile",
+				number : "111-222-3333"
+			}
+		],
+		emailAddresses : [
+			{
+				label : "work",
+				email : "john.smith@work.com"
+			}
+		]
+	},
+	{
+		recordID : 2,
+		familyName : "Doe",
+		givenName : "Jane",
+		phoneNumbers : [
+			{
+				label : "mobile",
+				number : "222-333-4444"
+			}
+		],
+		emailAddresses : [
+			{
+				label : "personal",
+				email : "jane.doe@me.com"
+			}
+		]
+	},
+	{
+		recordID : 3,
+		familyName : "Johansson",
+		givenName : "Scarlett",
+		phoneNumbers : [
+			{
+				label : "mobile",
+				number : "666-777-8888"
+			}
+		],
+		emailAddresses : [
+			{
+				label : "work",
+				email : "scarlett.johansson@hollywood.com"
+			}
+		]
+	}
+];
 
-AppRegistry.registerComponent('agora', () => agora);
+var styles = StyleSheet.create({
+	container : {
+		flex : 1,
+		justifyContent : "center"
+	},
+	listView : {
+		backgroundColor : "#EEEEEE"
+	},
+	listItem : {
+		paddingTop : 10,
+		paddingBottom : 10,
+		borderBottomWidth : 1
+	},
+	name : {
+		color : "#6699AA",
+		fontWeight : "bold"
+	},
+	phone : {
+		fontSize : 12
+	},
+	email : {
+		fontSize : 12
+	}
+})
+
+class Agora extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			error : {
+				message : "something went wrong"
+			},
+			dataSource : new ListView.DataSource({ 
+				rowHasChanged: (row1, row2) => row1 !== row2, 
+			}),
+			loaded : false
+		};
+	}
+	
+	componentDidMount() {
+		this.getContacts();
+	}
+	
+	getContacts() {
+		// prefetch contact list
+		/*ContactsData.getAll((err, contacts) => {
+			if(err && err.type === 'permissionDenied'){
+				this.setState({
+					error : {
+						message : "error fetching contacts"
+					}
+				});
+			} else {
+				this.setState({
+					contactList : contacts
+				});
+			}
+		});*/
+		this.setState({
+			dataSource : this.state.dataSource.cloneWithRows(FAKE_DATA),
+			loaded : true
+		})
+	}
+	
+	renderFailedView(error) {
+		return (
+			<View style={styles.container}>
+				<Text>{error.message}</Text>
+			</View>
+		)
+	}
+	
+	renderContact(contact) {
+		return (
+			<View style={styles.listItem}>
+				<Text style={styles.name}>{contact.givenName} {contact.familyName}</Text>
+				<Text style={styles.phone}>{contact.phoneNumbers[0].number}</Text>
+				<Text style={styles.email}>{contact.emailAddresses[0].email}</Text>
+			</View>
+		)
+	}
+	
+	renderContactList() {
+		return (
+			<View style={styles.container}>
+				<ListView 
+					dataSource={this.state.dataSource} 
+					renderRow={this.renderContact} 
+					style={styles.listView} 
+				/>
+			</View>
+		)
+	}
+	
+	render() {
+		if (!this.state.loaded) {
+			console.log("failed");
+			return this.renderFailedView(this.state.error);
+		}		
+		else {
+			console.log("success");
+			return this.renderContactList();
+		}
+	}
+};
+
+AppRegistry.registerComponent('agora', () => Agora);
